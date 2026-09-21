eab9ec5e699402808974ab3c122b3f771def50b5
Anshu-1606 <singhanshukumar1606@gmail.com>
2026-09-21 15:15:08 +0530

    Delete celsius_to_fahrenheit.py

--- celsius_to_fahrenheit.py
+++
@@ -1,8 +0,0 @@
-def celsius_to_fahrenheit(c):
-    f = (c * 9 / 5) + 32
-    return f
-
-
-c = float(input("Enter temperature in Celsius: "))
-
-print("Temperature in Fahrenheit:", celsius_to_fahrenheit(c))
