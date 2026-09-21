7326b02a1ab4b3ae3f8eb823326da0482cb11fb8
Anshu-1606 <singhanshukumar1606@gmail.com>
2026-09-21 15:06:06 +0530

    Add files via upload

---
+++ add_dict.py
@@ -0,0 +1,10 @@
+def add_entry(d):
+    d["age"] = 20
+
+data = {"name": "Anshu"}
+
+print("Before:", data)
+
+add_entry(data)
+
+print("After:", data)
---
+++ reassign.py
@@ -0,0 +1,11 @@
+
+def reassign_dict(d):
+    d = {"name": "Rahul", "age": 25}
+
+data = {"name": "Anshu"}
+
+print("Before:", data)
+
+reassign_dict(data)
+
+print("After:", data)
